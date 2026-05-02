3c0249233a2bfd624745c0bbe7a67f4dda2171d1
Bhoomika. <bhoomikulal735@gmail.com>
2026-05-02 19:01:52 +0530

    changed user interface

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-// Smooth scroll for navbar links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({ behavior: "smooth" });
-  });
-});
+const text = ["MERN Developer", "Problem Solver", "Tech Enthusiast"];
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  if (i < text.length) {
+    if (!isDeleting && j <= text[i].length) {
+      current = text[i].substring(0, j++);
+    } else if (isDeleting && j >= 0) {
+      current = text[i].substring(0, j--);
+    }
+
+    document.getElementById("typing").innerHTML = current;
+
+    if (j === text[i].length) isDeleting = true;
+    if (j === 0) {
+      isDeleting = false;
+      i++;
+    }
+  } else {
+    i = 0;
+  }
+
+  setTimeout(type, 100);
+}
+
+type();
